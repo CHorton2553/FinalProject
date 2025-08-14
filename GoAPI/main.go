407811652a1ae6653fa0c52d2a4f6c8e41c3377c
Casey Horton <cjh1295@gmail.com>
2025-08-14 07:45:04 -0400

    api framework complete

--- GoAPI/main.go
+++ GoAPI/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type note struct {
+	ID    string `json:"id"`
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
 
-var notes = []note{}
+var notes = []note{
+	{ID: "1", Title: "Homework", Body: "Math\nScience\nOrchestra"},
+}
 
 // create note with new note data
 func createNote(c *gin.Context) {
@@ -25,16 +29,32 @@ func createNote(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newNote)
 }
 
-// function: get note
-func getNote(c *gin.Context)
+func noteByID(c *gin.Context) {
+	id := c.Param("id")
+	note, err := getNoteByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Note not found."})
+	}
+	c.IndentedJSON(http.StatusOK, note)
+}
 
-// function: create folder
+func getNoteByID(id string) (*note, error) {
+	for i, n := range notes {
+		if n.ID == id {
+			return &notes[i], nil
+		}
+	}
+	return nil, errors.New("note not found")
+}
 
-// function: add to library
-
-// function: get library
+func getNotes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, notes)
+}
 
 func main() {
 	router := gin.Default()
 	router.POST("/notes", createNote)
+	router.GET("/notes", getNotes)
+	router.GET("/notes/:id", noteByID)
+	router.Run("localhost:8080")
 }
