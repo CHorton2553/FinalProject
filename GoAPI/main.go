606c25952efd7ed4627f27684d16d99e33e13b9b
Casey Horton <cjh1295@gmail.com>
2025-08-14 07:18:21 -0400

    install gin

--- GoAPI/main.go
+++ GoAPI/main.go
@@ -1,12 +1,10 @@
 package main
 
-/*
 import (
-	"errors"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
-*/
 
 type note struct {
 	Title string `json:"title"`
@@ -14,3 +12,29 @@ type note struct {
 }
 
 var notes = []note{}
+
+// create note with new note data
+func createNote(c *gin.Context) {
+	var newNote note
+	// bind the JSON data of new note to newNote
+	if err := c.BindJSON(&newNote); err != nil {
+		return
+	}
+	// add to notes
+	notes = append(notes, newNote)
+	c.IndentedJSON(http.StatusCreated, newNote)
+}
+
+// function: get note
+func getNote(c *gin.Context)
+
+// function: create folder
+
+// function: add to library
+
+// function: get library
+
+func main() {
+	router := gin.Default()
+	router.POST("/notes", createNote)
+}
